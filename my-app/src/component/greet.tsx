c70b8e3ed695fe97ae880a10c996c975ec2a0a31
Vandana nayak <vandana.nayak@novaders.com>
2022-11-21 12:26:17 +0530

    state hook

--- my-app/src/component/greet.tsx
+++ my-app/src/component/greet.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 let Greet = ((props: any) => {
+    const [count, setCount] = useState(0);
     // props.name='Pranav' this is wrong can't change the readonly props. they are immutable
-    // return <h1>Hello, {props.name}</h1>
-
     //multiple children in one div container
+    const increment = (() => {
+        if (count < 10) {
+            setCount(count + 1);
+        }
+    })
+    const decrement = (() => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    })
     return (
         <div>
             <h1>Hello, {props.name}</h1>
             {props.children}
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <p>{count}</p>
         </div>
     )
 })
