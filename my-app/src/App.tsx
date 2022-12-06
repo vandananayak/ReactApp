9bb527f7f528187f436b499fbc87a6079093c548
Vandana nayak <vandana.nayak@novaders.com>
2022-12-06 12:55:34 +0530

    routing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,12 +3,23 @@ import Arithmetic_ops from "./component/arithmetic_ops";
 import './App.css';
 import DisplayName from "./component/displayName";
 import MultipleInput from "./component/multipleInput";
+import {BrowserRouter as Router, Routes, Route, Link, useNavigate} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <DisplayName />
-            <MultipleInput/>
+            <Router>
+                <div>
+                    <Link to="/" className="btn btn-primary">Name</Link>
+                    <Link to="/multiple" className="btn btn-primary">Full Name</Link>
+                </div>
+                <Routes>
+                    <Route path={'/'} element={<DisplayName/>}></Route>
+                    <Route path={'/multiple'} element={<MultipleInput/>}></Route>
+                </Routes>
+            </Router>
+
+
         </div>
     );
 }
